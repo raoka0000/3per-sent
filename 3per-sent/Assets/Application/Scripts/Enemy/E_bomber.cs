46a5884738c751265cf2e52dff7a9ebdfaccfa2b
raoka0000 <raoka0000@gmail.com>
2017-11-02 05:57:02 +0900

    ボムを使う敵の実装

--- 3per-sent/Assets/Application/Scripts/Enemy/E_bomber.cs
+++ 3per-sent/Assets/Application/Scripts/Enemy/E_bomber.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using PlayScene;
 
 namespace PlayScene.Enemys{
 
-public class E_heal : Enemy {
+public class E_bomber : Enemy {
 
 	private GameObject target;
 	private float angle = 0;
@@ -13,37 +14,49 @@ public class E_heal : Enemy {
 	[SerializeField]
 	private float speed = 1;
 	[SerializeField]
-	private float shotInterval = 1;
+	private float explosionInterval = 3f;
 
+
+	private int stete = 0;
+	private Vector3 defaultScale = new Vector3(0.5f,0.5f,1);
 
 	protected override void Init(){
-		
+		stete = 0;
+		this.transform.localScale = defaultScale;
 	}
 	public override void killed(){
+		this.transform.DOKill ();
 		ParticleManager.instance.HitEffect (transform,15);
 		AudioManager.instance.PlaySE (DEFINE.SHOT_SE,DEFINE.SHOT_SE_CH);
 	}
 	private float time = 0;
 	private Vector3 tmpvec = Vector3.one;
 	protected override void _Update () {
-		if (target == null) {
-			target = SetTarget (TagUtil.GetFriendTag(this.tag));
-		} else {
-			tmpvec = target.transform.position;
-			tmpvec.x -= 1;
-			tmpvec.y += 1;
-			angle = GetAim (tmpvec);
-			tmp.x = Mathf.Cos (angle * Mathf.Deg2Rad);
-			tmp.y = Mathf.Sin (angle * Mathf.Deg2Rad);
-			tmpvec = transform.position + (tmp * speed * myDeltaTime);
-			if (Vector3.Distance (tmpvec, target.transform.position) > 3.0f) {
+		if (stete == 0) {
+			if (target == null) {
+				target = SetTarget (TagUtil.GetHomingActorTag(this.tag));
+			} else {
+				angle = GetAim (target.transform.position);
+				tmp.x = Mathf.Cos (angle * Mathf.Deg2Rad);
+				tmp.y = Mathf.Sin (angle * Mathf.Deg2Rad);
+				tmpvec = transform.position + (tmp * speed * myDeltaTime);
 				transform.position = tmpvec;
+				if (Vector3.Distance (transform.position, target.transform.position) < 3.0f) {
+					stete = 1;
+				}
 			}
 		}
-		time += myDeltaTime;
-		if(time > shotInterval){
-			bulletUnits.Shot (0);
-			time = 0;
+		if(stete == 1){
+			tmp.x = 2;
+			tmp.y = 2;
+			this.transform.DOScale (tmp, explosionInterval).SetEase (Ease.InBounce)
+				.OnKill(
+					()=>{
+						bulletUnits.Shot (0);
+						DOVirtual.DelayedCall (0.01f, ()=>this.Damaged(this.maxHp));
+					}
+				);
+			stete = 2;
 		}
 	}
 
