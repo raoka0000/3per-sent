3533e88812472107d1b9b946f533c70661bc7c0f
Sawada Masahiro <0av1k50f785476h@ezweb.ne.jp>
2017-11-04 17:17:16 +0900

    蜘蛛兵士２号動き実装しました。（変数変更)

--- 3per-sent/Assets/Application/Scripts/Enemy/E_spiderwarrior_var2.cs
+++ 3per-sent/Assets/Application/Scripts/Enemy/E_spiderwarrior_var2.cs
@@ -11,13 +11,13 @@ public class E_spiderwarrior_var2 : Enemy{
 
 
 	[SerializeField]
-	private  static float speed_y = -2.0f;
+	private float speed_y = -4.0f;
 	[SerializeField]
 	private float wait_time =0.0f;
-	private float st_speed=speed_y;
+	private float st_speed = 0;
 
 	protected override void Init (){
-		 
+		st_speed = speed_y;
 		/*this.transform.DOLocalMoveY(speed_y,3f).OnComplete(()=>{
 			Debug.Log("speedy " + speed_y);
 			bulletUnits.Shot (0);
@@ -40,7 +40,7 @@ public class E_spiderwarrior_var2 : Enemy{
 		if(this.transform.position.y<0){
 			speed_y=0.0f;
 			wait_time+=myDeltaTime;
-			if(wait_time>=2.0f){
+			if(wait_time>=1.0f){
 				speed_y=st_speed*-1;
 				Debug.Log(speed_y);
 			}
